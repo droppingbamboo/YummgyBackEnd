f9cfd8b90be85252f766a57e9ff30e95d73242f5
Brionne <63943029+droppingbamboo@users.noreply.github.com>
2023-12-11 13:43:30 -0800

    Boilerplate added

---
+++ Yummgy/src/main/java/com/cognixia/jump/YummgyApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummgyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummgyApplication.class, args);
+	}
+
+}
