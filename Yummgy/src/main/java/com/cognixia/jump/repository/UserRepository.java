5a14fbf00c95447162614407962699a44c999dfa
abashir191 <abdullahim.mohamed0@gmail.com>
2023-12-12 13:12:17 -0600

    updated user repo

--- Yummgy/src/main/java/com/cognixia/jump/repository/UserRepository.java
+++ Yummgy/src/main/java/com/cognixia/jump/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package com.cognixia.jump.repository;
 
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.cognixia.jump.model.User;
@@ -14,6 +12,5 @@ import com.cognixia.jump.model.User;
 public interface UserRepository extends JpaRepository<User, Integer>{
 	public List<User> findAll();
 	
-    @Query("SELECT u FROM User u LEFT JOIN FETCH u.favorites WHERE u.userId = :userId")
-    Optional<User> findByIdWithFavorites(@Param("userId") Integer userId);
+
 }
