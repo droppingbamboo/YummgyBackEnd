fbf2ac2206e4b114c00eeec878616e37de8fd3a5
abashir191 <abdullahim.mohamed0@gmail.com>
2023-12-12 12:12:38 -0600

    added user crud operations

--- Yummgy/src/main/java/com/cognixia/jump/repository/UserRepository.java
+++ Yummgy/src/main/java/com/cognixia/jump/repository/UserRepository.java
@@ -1,11 +1,19 @@
 package com.cognixia.jump.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.cognixia.jump.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
-
+	public List<User> findAll();
+	
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.favorites WHERE u.userId = :userId")
+    Optional<User> findByIdWithFavorites(@Param("userId") Integer userId);
 }
