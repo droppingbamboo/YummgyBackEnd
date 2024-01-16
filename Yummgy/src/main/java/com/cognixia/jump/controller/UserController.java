7e38b62ff3b18b1593ec1e935ce0b12d0358a5ce
Grant Smith <bomono8@gmail.com>
2024-01-16 14:40:36 -0600

    Added api calls for enabled, locked, expired, and credentials bad.

--- Yummgy/src/main/java/com/cognixia/jump/controller/UserController.java
+++ Yummgy/src/main/java/com/cognixia/jump/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -278,6 +279,98 @@ public class UserController {
 	@GetMapping("/users/loggedin")
 	public ResponseEntity<?> getLoggedInUser(@RequestHeader (name="Authorization") String token) {
 		return ResponseEntity.status(200).body(jwtUtil.getLoggedInUser(token));
+	}
+	
+	@Operation(summary = "Toggle the enabled value of a user",
+			description = "Swaps the current enabled value of a user by id if the request is done by an admin.")
+	@ApiResponses({
+			@ApiResponse(responseCode="200",
+			description="Enabled has been swapped"),
+			@ApiResponse(responseCode="400",
+			description="User does not exist")}
+	)
+	@CrossOrigin
+	@PatchMapping("/admin/user/security/enabled/{userId}")
+	public ResponseEntity<?> toggleEnabled(@PathVariable Integer userId) throws ResourceNotFoundException {
+		Optional<User> userOptional = repo.findById(userId);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setEnabled(!user.isEnabled());
+            user = repo.save(user);
+            return ResponseEntity.status(200).body(user.isEnabled());
+        } else {
+        	throw new ResourceNotFoundException("User", userId);
+        }
+	}
+	
+	@Operation(summary = "Toggle the locked value of a user",
+			description = "Swaps the current locked value of a user by id if the request is done by an admin.")
+	@ApiResponses({
+			@ApiResponse(responseCode="200",
+			description="Locked has been swapped"),
+			@ApiResponse(responseCode="400",
+			description="User does not exist")}
+	)
+	@CrossOrigin
+	@PatchMapping("/admin/user/security/locked/{userId}")
+	public ResponseEntity<?> toggleLocked(@PathVariable Integer userId) throws ResourceNotFoundException {
+		Optional<User> userOptional = repo.findById(userId);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setLocked(!user.isLocked());
+            user = repo.save(user);
+            return ResponseEntity.status(200).body(user.isLocked());
+        } else {
+        	throw new ResourceNotFoundException("User", userId);
+        }
+	}
+	
+	@Operation(summary = "Toggle the expired value of a user",
+			description = "Swaps the current expired value of a user by id if the request is done by an admin.")
+	@ApiResponses({
+			@ApiResponse(responseCode="200",
+			description="Enabled has been swapped"),
+			@ApiResponse(responseCode="400",
+			description="User does not exist")}
+	)
+	@CrossOrigin
+	@PatchMapping("/admin/user/security/expired/{userId}")
+	public ResponseEntity<?> toggleExpired(@PathVariable Integer userId) throws ResourceNotFoundException {
+		Optional<User> userOptional = repo.findById(userId);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setExpired(!user.isExpired());
+            user = repo.save(user);
+            return ResponseEntity.status(200).body(user.isExpired());
+        } else {
+        	throw new ResourceNotFoundException("User", userId);
+        }
+	}
+	
+	@Operation(summary = "Toggle the credentialsBad value of a user",
+			description = "Swaps the current credentialsBad value of a user by id if the request is done by an admin.")
+	@ApiResponses({
+			@ApiResponse(responseCode="200",
+			description="credentialsBad has been swapped"),
+			@ApiResponse(responseCode="400",
+			description="User does not exist")}
+	)
+	@CrossOrigin
+	@PatchMapping("/admin/user/security/credentials/{userId}")
+	public ResponseEntity<?> toggleCredentials(@PathVariable Integer userId) throws ResourceNotFoundException {
+		Optional<User> userOptional = repo.findById(userId);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setCredentialsBad(!user.isCredentialsBad());
+            user = repo.save(user);
+            return ResponseEntity.status(200).body(user.isCredentialsBad());
+        } else {
+        	throw new ResourceNotFoundException("User", userId);
+        }
 	}
 	
 }
